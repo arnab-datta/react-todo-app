c0ee2f5b1ebc03746015c3b9033915876d154d7f
arnab-datta <dattaarnab2013@gmail.com>
2020-09-12 19:00:24 +0530

    Changin from class based component to Functional approach

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import React from "react";
-import "./App.css";
-// import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import React, { Component, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Swal from "sweetalert2";
 import "./App.css";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      taskItem: "",
-      list: [],
-    };
-  }
+function App() {
+  const [taskItem, setTaskItem] = useState("");
+  const [taskList, setTaskList] = useState([]);
 
-  addTodoItem(todoValue) {
+  const addTodoItem = (todoValue, e) => {
+    e.preventDefault();
     let val = todoValue.replace(/[^a-zA-Z0-9- ]/g, "");
     if (val.trim() !== "") {
       const newItem = {
@@ -21,107 +16,103 @@ class App extends React.Component {
         value: val,
         isDone: false,
       };
-      const todoList = [...this.state.list];
+      const todoList = [...taskList];
       todoList.push(newItem);
-      this.setState({
-        list: todoList,
-        taskItem: "",
-      });
+      setTaskItem("");
+      setTaskList(todoList);
     } else {
-      alert("failed to add");
+      setTaskItem("");
+      Swal.fire({
+        icon: "error",
+        title: "Invalid or Empty Task",
+        focusConfirm: false,
+        confirmButtonColor: "#2b2d5d",
+      });
     }
-  }
+  };
 
-  deleteTodoItem(delTaskId) {
-    const todoList = [...this.state.list];
+  const deleteTodoItem = (delTaskId) => {
+    const todoList = [...taskList];
     const updatedtodoList = todoList.filter((item) => item.id !== delTaskId);
-    this.setState({
-      list: updatedtodoList,
-    });
-  }
+    setTaskList(updatedtodoList);
+  };
 
-  updateInput(inputTask) {
-    this.setState({
-      taskItem: inputTask,
-    });
-  }
+  const updateInput = (inputTask) => {
+    setTaskItem(inputTask);
+  };
 
-  handleCheck(isChecked, taskId) {
-    let todoList = [...this.state.list];
+  const handleCheck = (isChecked, taskId) => {
+    let todoList = [...taskList];
     const foundItemIndex = todoList.findIndex((item) => item.id === taskId);
     todoList[foundItemIndex].isDone = isChecked;
-    this.setState({
-      list: todoList,
-    });
-  }
+    setTaskList(todoList);
+  };
 
-  render() {
-    return (
-      <div>
-        <div className="container">
-          <form className="frmMrgn">
-            <div className="form-group">
-              <h1 className="text-center">Todo App</h1>
-              <div className="input-group-prepend centerAlign">
-                <input
-                  type="search"
-                  className="form-control"
-                  placeholder="What to do ??"
-                  required
-                  value={this.state.taskItem}
-                  onChange={(e) => this.updateInput(e.target.value)}
-                />
-                <button
-                  className="btn buttonCustom btnColPinkShade"
-                  onClick={() => this.addTodoItem(this.state.taskItem)}
-                  disabled={!this.state.taskItem.length}
-                >
-                  Add TODO
-                </button>
-              </div>
+  return (
+    <div>
+      <div className="container">
+        <form className="frmMrgn">
+          <div className="form-group">
+            <h1 className="text-center">Todo App</h1>
+            <div className="input-group-prepend centerAlign">
+              <input
+                type="search"
+                className="form-control"
+                placeholder="What to do ??"
+                required
+                value={taskItem}
+                onChange={(e) => updateInput(e.target.value)}
+              />
+              <button
+                className="btn buttonCustom btnColPinkShade"
+                onClick={(e) => addTodoItem(taskItem, e)}
+                disabled={!taskItem.length}
+              >
+                Add TODO
+              </button>
             </div>
-          </form>
-
-          <div className="cardParent">
-            <ul className="list-group">
-              {this.state.list.map((item) => {
-                return (
-                  <li
-                    key={item.id}
-                    className="list-group-item d-flex  centerAlign lstItem"
-                  >
-                    <input
-                      type="checkbox"
-                      name="isDone"
-                      checked={item.isDone}
-                      onChange={(e) => {
-                        this.handleCheck(e.target.checked, item.id);
-                      }}
-                    />
-                    <div className="tt">{item.value}</div>
-                    <span
-                      className={`badge badge-pill customBdg ${
-                        item.isDone === false ? "badge-danger" : "badge-success"
-                      }`}
-                    >
-                      {`${item.isDone === false ? `pending` : "Done"}`}
-                    </span>
-                    <button
-                      className="btn btn-danger buttonCustom"
-                      disabled={!item.isDone}
-                      onClick={() => this.deleteTodoItem(item.id)}
-                    >
-                      Delete
-                    </button>
-                  </li>
-                );
-              })}
-            </ul>
           </div>
+        </form>
+
+        <div className="cardParent">
+          <ul className="list-group">
+            {taskList.map((item) => {
+              return (
+                <li
+                  key={item.id}
+                  className="list-group-item d-flex  centerAlign lstItem"
+                >
+                  <input
+                    type="checkbox"
+                    name="isDone"
+                    checked={item.isDone}
+                    onChange={(e) => {
+                      handleCheck(e.target.checked, item.id);
+                    }}
+                  />
+                  <div className="tt">{item.value}</div>
+                  <span
+                    className={`badge badge-pill customBdg ${
+                      item.isDone === false ? "badge-danger" : "badge-success"
+                    }`}
+                  >
+                    {`${item.isDone === false ? `pending` : "Done"}`}
+                  </span>
+                  <button
+                    className="btn btn-danger buttonCustom"
+                    disabled={!item.isDone}
+                    onClick={() => deleteTodoItem(item.id)}
+                  >
+                    Delete
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
